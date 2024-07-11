47434fbabcb55a0d9dda9e404766ffd42063d171
ArseniPav <sanchoars@gmail.com>
2024-07-11 19:13:37 +0200

    init commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,6 +6,10 @@ package org.example;
  */
 public class App
 {
+    public static void fibNumber(){
+        System.out.println("TestFetch");
+    }
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
